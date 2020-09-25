8c10c1635397a92b429dc86ef52baab432105824
한혜경 <betty@hanhyegyeong-ui-MacBookPro.local>
2020-09-26 01:20:44 +0900

    :sparkles: video upload 기능 추가

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,27 +1,38 @@
-import { videoData } from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videoData });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videoData });
+  const videos = await Video.find({});
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   // TODO: video info save
   const {
-    body: { file, title, description },
+    file: { path },
+    body: { title, description },
   } = req;
-  res.redirect(routes.videoDetail(1234545));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const editVideo = (req, res) => {
